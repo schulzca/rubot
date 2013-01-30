ee5483b0f20b1c04be60987d24126a5f7fe21721
schulzca <coreyaschulz@gmail.com>
2013-01-30 16:01:47 -0600

    Initial commit of summon

---
+++ minion.rb
@@ -0,0 +1,75 @@
+require 'rubygems'
+require 'cinch'
+require 'cinch/plugins/identify'
+require 'yaml'
+
+def random_name
+  constants = %w(b c d f g h j k l m n p qu r s t v w x y z ch st tr pl)
+  vowels = %w(a e i o u y ie au ea)
+  name = ""
+  5.times do |i|
+    name += (i % 2 == 0 ? constants.shuffle.first : vowels.shuffle.first)
+  end
+  name
+end
+
+begin
+  nick = random_name
+  $settings = YAML.load(File.read("minion.yml"))
+  $settings["settings"]["channel"] = [ARGV.first]
+  $settings["settings"]["nick"] = nick
+end
+
+$master = $settings["settings"]["master"]
+
+# This method is taken from rails core
+# (didn't want to load the entire lib for one method)
+# http://api.rubyonrails.org/classes/ActiveSupport/Inflector.html#method-i-constantize
+def constantize(camel_cased_word)
+  names = camel_cased_word.split('::')
+  names.shift if names.empty? || names.first.empty?
+
+  constant = Object
+  names.each do |name|
+    constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+  end
+  constant
+end
+
+$help_messages = []
+
+if $settings["settings"]["abilities"]
+  $settings["settings"]["abilities"].each do |plugin|
+    require "./abilities/#{plugin}"
+  end
+end
+
+$irc  = Cinch::Bot.new do
+  
+  configure do |c|
+    c.server = "irc.freenode.com"
+    c.nick = $settings["settings"]["nick"]
+    c.channels = $settings["settings"]["channel"]
+    c.plugins.plugins = $settings["settings"]["plugins"].map {|plugin| constantize(plugin.split("_").map {|word| word.capitalize}.join(""))} if $settings["settings"]["plugins"]
+  end
+
+  on :message, /^!help$/ do |m|
+    $help_messages.each{|message| m.user.send message }
+  end
+
+  on :message, /^!unsummon$/ do |m|
+    if m.user == User($master)
+      $irc.quit
+      system("exit")
+    end
+  end
+
+  on :message, /^!unsummon #{$settings["settings"]["nick"]}$/ do |m|
+    if m.user == User($master)
+      $irc.quit
+      system("exit")
+    end
+  end
+end
+
+$irc.start
---
+++ plugins/summon.rb
@@ -0,0 +1,35 @@
+class Summon < PluginBase
+  include Cinch::Plugin
+
+	$help_messages << "!summon x    Master summon's x minions (default 1)."
+
+	listen_to :channel
+	listen_to :private
+	
+	def listen(m)
+			begin
+				case m.message
+				when /^!summon help$/
+					help(m, "!summon")
+        when /^!summon$/
+          summon(m,1,m.channel)
+        when /^!summon(\s\d+)?(\s\S+)?$/
+          summon(m,$1,$2.strip)
+  			end
+			rescue Exception => e
+				error(m,e)
+			end
+	end
+	
+	def summon(m,number,channel)
+	  number = number ? number.to_i : 1
+	  if m.user == User($master) && channel
+	    $settings["settings"]["channel"].each do |c|
+        @channel = c if c.match(/^#{Regexp.escape(channel)}/)
+      end
+      number.times do
+        system("ruby minion.rb '#{@channel}' &")
+      end
+    end
+	end
+end
