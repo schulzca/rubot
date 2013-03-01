4e7b1e4d8c2b59119f52e3954fac60202aa8648a
schulzca <coreyaschulz@gmail.com>
2013-03-01 14:11:47 -0600

    Let plugin base manage the broadcast

--- plugins/google_that.rb
+++ plugins/google_that.rb
@@ -26,22 +26,11 @@ class GoogleThat < PluginBase
     end
 	end
 	
-	def track_channels(m)
-    @@channels[m.channel.to_s] = m
-  end
-
 	def generate_link(m,user,message)
 	  url = "http://lmgtfy.com?q=#{message}"
 		url = open("http://tinyurl.com/api-create.php?url=#{URI.escape(url)}").read
 		if url != "Error"
-      @@channels.each do |c,m2|
-        if m2.channel
-          users = m2.channel.users.collect{|u| u.first.nick}
-          if users.include? m.user.nick and users.include? user
-            reply m2,"#{user}: #{url}"
-          end
-        end
-      end
+      broadcast(m,user,url)
     end
 	end
 end
